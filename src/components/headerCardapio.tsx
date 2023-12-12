4faf4db6f00426071254264039d34f87ceb084d7
Luis Felipe <degaus@live.com>
2023-12-11 22:40:36 -0300

    change image bg

--- src/components/headerCardapio.tsx
+++ src/components/headerCardapio.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 // import { Container } from './styles';
 
 const HeaderCardapio: React.FC = () => {
   return (
-    <div className='w-full bg-amber-200 h-[200px] mt-3 rounded-3xl relative'>
-      <button className='absolute bottom-4 right-2 bg-white px-4 py-2'>Informações</button>
+    <div className="w-full bg-amber-200 h-[200px] mt-3 rounded-3xl relative bg-[url('/images/hamburguer.png')] bg-auto bg-no-repeat bg-center">
+      <button className="absolute bottom-4 right-2 bg-white px-4 py-2 rounded-[80px] font-semibold cursor-pointer">
+        Informações
+      </button>
     </div>
   );
-}
+};
 
 export default HeaderCardapio;
