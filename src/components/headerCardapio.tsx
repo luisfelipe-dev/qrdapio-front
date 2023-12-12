2ce42d41ee258a8e4321b1ad9f2922a1395aab6e
Luis Felipe <degaus@live.com>
2023-12-11 22:33:07 -0300

    init btn modal

--- src/components/headerCardapio.tsx
+++ src/components/headerCardapio.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 // import { Container } from './styles';
 
 const HeaderCardapio: React.FC = () => {
-  return <h1>Header</h1>;
+  return (
+    <div className='w-full bg-amber-200 h-[200px] mt-3 rounded-3xl relative'>
+      <button className='absolute bottom-4 right-2 bg-white px-4 py-2'>Informações</button>
+    </div>
+  );
 }
 
 export default HeaderCardapio;
