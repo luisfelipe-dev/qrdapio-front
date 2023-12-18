ef1e3abf0ffeccdc0dbf119e58772f2518481605
Luis Felipe <degaus@live.com>
2023-12-18 20:26:00 -0300

    feat: modal

--- src/components/headerCardapio.tsx
+++ src/components/headerCardapio.tsx
@@ -1,4 +1,13 @@
+"use client";
 import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 type HeaderCardapioProps = {
   URLImage: string;
@@ -12,9 +21,20 @@ function HeaderCardapio({ URLImage }: HeaderCardapioProps) {
       }}
       className={`w-full bg-amber-200 h-[200px] mt-3 rounded-3xl relative bg-auto bg-no-repeat bg-center mb-6`}
     >
-      <button className="absolute bottom-4 right-4 bg-white px-4 py-2 rounded-[80px] font-semibold cursor-pointer">
-        Informações
-      </button>
+      <Dialog>
+        <DialogTrigger className="absolute bottom-4 right-4 bg-white px-4 py-2 rounded-[80px] font-semibold cursor-pointer">
+          Informações
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Sobre</DialogTitle>
+            <DialogDescription>
+              conteudo aqui
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      {/* </button> */}
     </div>
   );
 }
