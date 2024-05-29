544093212086769d446538cb5aca5b32838a354e
Luis Felipe <degaus@live.com>
2024-05-29 19:50:35 -0300

    feat: teste deploy 2

--- src/app/registre/page.tsx
+++ src/app/registre/page.tsx
@@ -1,15 +1,12 @@
 "use client";
-// import RegisterForm from "@/components/RegisterForm";
+import RegisterForm from "@/components/RegisterForm";
 import { ContainerGrid } from "@/components/container";
 import React from "react";
-
-// import { Container } from './styles';
 
 const Register: React.FC = () => {
   return (
     <ContainerGrid className="h-lvh flex flex-wrap items-center justify-center">
-      {/* <RegisterForm /> */}
-      <p>teste deploy</p>
+      <RegisterForm />
     </ContainerGrid>
   );
 };
